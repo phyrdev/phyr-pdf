60b3e355a849c7432205ad486e271e4d36fb1b6a
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-06-18 23:17:35 +0530

    chore: Add MQTT functionality for animalize server

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const animalize = require("./routes/animalize");
 const cors = require("cors");
+const mqtt = require("mqtt");
+
+try {
+  const client = mqtt.connect("wss://test.mosquitto.org:8081");
+  client.on("connect", () => {
+    console.log("Connected to MQTT broker.");
+    client.subscribe("animalize/server");
+    client.on("message", (topic, message) => {
+      console.log(message.toString());
+      client.publish("animalize/client", "Hello from server.");
+    });
+  });
+} catch (error) {
+  console.log("Error connecting to MQTT broker.");
+}
 
 app.use(
   cors({
@@ -13,6 +28,7 @@ app.use(
     optionsSuccessStatus: 204,
   })
 );
+
 app.use(express.json());
 app.use("/animalize", animalize);
 app.use("/public", express.static(path.join(__dirname, "/public")));
