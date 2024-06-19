112a9f200e75ab0405ee4579b3582f6eb0cbe424
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-06-19 16:29:54 +0530

    mqtt integration

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const path = require("path");
 const port = process.env.PORT || 3001;
 const animalize = require("./routes/animalize");
 const cors = require("cors");
+const mqtt = require("mqtt");
+const randomstring = require("randomstring");
+
+try {
+  const id = randomstring.generate(5);
+  const client = mqtt.connect("mqtt://broker.hivemq.com");
+  client.on("connect", () => {
+    console.log("Connected to MQTT broker");
+    client.subscribe("animalize/server");
+  });
+  client.on("message", (topic, message) => {
+    console.log(`Received message on ${topic}: ${message.toString()}`);
+    client.publish("animalize/client", `Hello from Node.js with id ${id}`);
+  });
+} catch (error) {
+  console.log(error);
+}
 
 app.use(
   cors({
